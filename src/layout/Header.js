4cd115592fca865ffc4b606a95148d15b6a28d57
Tringapps-Saranya <102800505+Tringapps-Saranya@users.noreply.github.com>
2022-05-17 13:59:45 +0530

    Update Header.js

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,13 +1,3 @@
-// import React from 'react'
-// import '../asserts/Header.css'
-// import '../asserts/Layout.css'
-// export const Header = () => {
-//   return (
-//     <div className="header">
-//        <p>Welcome</p>
-//     </div>
-//   )
-// }
 import React from 'react'
 import '../asserts/Layout.css'
 import '../asserts/Header.css'
